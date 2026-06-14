116502a0e792b1419f0fef852788596c2de061eb
tuando05 <dotuan3108@gmail.com>
2026-06-14 15:30:54 +0700

    feat: bo sung cau hinh cho tinh nang Reranker

--- src/config/model.py
+++ src/config/model.py
@@ -4,6 +4,8 @@ class ModelConfig(BaseSettings):
     OLLAMA_URL: str
     LLM_MODEL: str
     EMBEDDING_MODEL_NAME: str
+    USE_RERANKER: bool = False
+    RERANKER_MODEL_NAME: str = "BAAI/bge-reranker-base"
 
     class Config:
         env_file = ".env"
